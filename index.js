b0807d321dc845efe124b6772b2654bf2715b03b
Mustakim <sycospike1@gmail.com>
2023-05-20 12:31:43 +0600

    added all task

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -24,6 +25,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true, message: 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err,decoded)=>{
+    if(err){
+      return res.status(401).send({error:true , message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -32,6 +48,17 @@ async function run() {
     const serviceCollection = client.db('carDoctor').collection('services');
     const bookingCollection = client.db('carDoctor').collection('bookings');
 
+    // jwt
+    app.post('/jwt', (req,res) => {
+      const user = req.body ;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({token});
+    })
+
+    // services routes
     app.get('/services', async(req,res) =>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
@@ -49,8 +76,12 @@ async function run() {
     })
 
     //bookings
-    app.get('/bookings', async(req,res) => {
-      console.log(req.query.email);
+    app.get('/bookings', verifyJWT,  async(req,res) => {
+      const decoded = req.decoded;
+      console.log('came back after verify', decoded);
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({error:1, message: 'forbidden access'})
+      }
       let query = {};
       if(req,query?.email){
         query = {email: req.query.email}
@@ -66,8 +97,18 @@ async function run() {
         res.send(result);
     })
 
-    app.put('/bookings/:id', async(req, res) => {
+    app.patch('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
       const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        }
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
     })
 
     app.delete('/bookings/:id', async(req, res) => {
