aeeb905f5953b91dff3864c1a3b0bf22624b3002
Mustakim <sycospike1@gmail.com>
2023-05-22 17:21:41 +0600

    added sort and search

--- index.js
+++ index.js
@@ -60,7 +60,16 @@ async function run() {
 
     // services routes
     app.get('/services', async(req,res) =>{
-        const cursor = serviceCollection.find();
+      const sort = req.query.sort;
+      const search = req.query.search;
+      //const query = {} ;
+      const query = {title: {$regex: search, $options: 'i'}}
+      const options = {
+        sort: {
+          'price': sort === 'asc' ? 1 : -1
+        }
+      }
+        const cursor = serviceCollection.find(query, options);
         const result = await cursor.toArray();
         res.send(result);
     })
